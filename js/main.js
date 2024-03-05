b74d2420519af3e6546d39d704ff6589349708af
andrii-cabbage <kapysta377@gmail.com>
2024-03-05 22:31:45 +0200

    hw

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// document.write("JS saying HI");
+// console.log("JS saying HI");
+// console.info("JS saying HI");
+// console.error("JS saying HI");
+// console.warn("JS saying HI");
+const num = 7;
+//num = 5;
+console.log("Переменная: " + num);
+
+var number;
+number = false;
+
+
+var nau_1 = 5;
+var nau_2 = "5";
