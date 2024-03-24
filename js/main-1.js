4ec6ce9efb275698ceaaee62f2e008c20ae399fc
andrii-cabbage <kapysta377@gmail.com>
2024-03-24 19:58:52 +0200

    hw

---
+++ js/main-1.js
@@ -0,0 +1,5 @@
+//for(let i = 100; i > 10; i /= 2) {
+//    console.log(i);
+    
+//}
+
---
+++ js/main-2.js
@@ -0,0 +1,13 @@
+let dataProj = [4, 5, 6, 34, 2123, 34.32, "Bold", true];
+dataProj[0] = "Saome";
+console.log(dataProj.length);
+
+
+let matrix = [
+    [44, 23, 54, 123, 43], 
+    ["World", false], 
+    [true, 44.5, 54.12]
+];
+
+matrix[0][0] = 60;
+console.log(matrix[2][1]);
